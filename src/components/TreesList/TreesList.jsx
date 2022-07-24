bd8e568c4a97dd1e0b284a861a83081ba6a1b201
Roman Havryliuk <gavru4r@gmail.com>
2022-07-24 03:57:49 +0300

    create modal for map

--- src/components/TreesList/TreesList.jsx
+++ src/components/TreesList/TreesList.jsx
@@ -1,20 +1,22 @@
 import { getFilterValue } from "../../redux/user/userSelector";
 import s from "./TreesList.module.scss";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { getUserEmail } from "../../redux/auth/authSelector";
-import { delUserContact, getContact } from "../../redux/user/userOperations";
+// import { delUserContact, getContact } from "../../redux/user/userOperations";
 
 import img from "../../images/favicon.png";
+import Modal from "../../components/Modal/Modal";
+import CardTree from "../../components/CardTree/CardTree";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Map, Marker, Overlay } from "pigeon-maps";
 import ReactTooltip from "react-tooltip";
 import "tippy.js/dist/tippy.css";
 
 const initalArr = [
-  { lat: 50.879, lang: 4.6997, nodeName: "sdfg" },
-  { lat: 50.888, lang: 4.6997, nodeName: "yyyy" },
-  { lat: 50.899, lang: 5.6997, nodeName: "yyyiuuh" },
+  { lat: 50.879, lang: 4.6997, nodeName: "sdfg", id: "45655665456" },
+  { lat: 50.888, lang: 4.6997, nodeName: "yyyy", id: "44555643222" },
+  { lat: 50.899, lang: 5.6997, nodeName: "yyyiuuh", id: "33222222222" },
 ];
 
 const TreesList = () => {
@@ -38,16 +40,40 @@ const TreesList = () => {
     setShowBtn(true);
   };
 
-  const dispatch = useDispatch();
+  const [modal, setModal] = useState({
+    open: false,
+    content: null,
+  });
 
-  useEffect(() => {
-    dispatch(getContact({ email: userEmail }));
-  }, []);
-
-  const delContact = async (id) => {
-    await dispatch(delUserContact({ contactId: id, email: userEmail }));
-    dispatch(getContact({ email: userEmail }));
+  const openModal = (content) => {
+    setModal({
+      open: true,
+      content,
+    });
   };
+
+  const closeModal = () => {
+    setModal({
+      open: false,
+      content: null,
+    });
+  };
+  const handleOpenModal = (id) => {
+    console.log("id :>> ", id);
+    // const email = e.currentTarget.id;
+    openModal("userInfo");
+  };
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getContact({ email: userEmail }));
+  // }, []);
+
+  // const delContact = async (id) => {
+  //   await dispatch(delUserContact({ contactId: id, email: userEmail }));
+  //   dispatch(getContact({ email: userEmail }));
+  // };
 
   return (
     <section className={s.treesSection}>
@@ -62,30 +88,23 @@ const TreesList = () => {
           }
         />
 
-        {initalArr.map(({ lat, lang, nodeName }) => (
-          // <div key={nodeName}>
+        {initalArr.map(({ lat, lang, nodeName, id }) => (
           <Marker
             width={30}
             anchor={[lat, lang]}
+            // value={}
             color="green"
             key={nodeName}
-            // hover={{ color: "red" }}
-            // hover={() => setModalInfo(true)}
-            onMouseOver={() => setModalInfo(true)}
-            onMouseOut={() => setModalInfo(false)}
+            onClick={() => handleOpenModal(id)}
+            // onMouseOver={() => setModalInfo(true)}
+            // onMouseOut={() => setModalInfo(false)}
           />
-          /* <p>gkjjgfkfgf</p> */
-          /* {modalInfo && (
-              <div anchor={[lat, lang]} offset={[120, 79]} key={nodeName}>
-                <img src="/img/pigeon.jpg" width={240} height={158} alt="" />
-              </div>
-            )} */
-          /* </div> */
         ))}
-
-        {/* < ReactTooltip id={nodeName} type="warning" effect="solid">
-          <span>Show sad face</span>
-        <ReactTooltip/> */}
+        {modal.open && (
+          <Modal handleClose={closeModal} checker={true}>
+            <CardTree contact={modal.content} closeModal={closeModal} />
+          </Modal>
+        )}
         {showInfo && (
           <Overlay anchor={anchor} offset={[120, 79]}>
             {payload}
