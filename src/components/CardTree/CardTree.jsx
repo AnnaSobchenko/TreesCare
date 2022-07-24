fb534bc8a08d76a941035956478b2dc41d83b4cd
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-07-24 10:21:16 +0300

    trees card

--- src/components/CardTree/CardTree.jsx
+++ src/components/CardTree/CardTree.jsx
@@ -3,18 +3,6 @@ import { getIsAdmin, getIsLoggedIn } from "../../redux/auth/authSelector";
 import { delUserById, getAllUsers } from "../../redux/user/userOperations";
 import s from "./CardTree.module.scss";
 const TreeImg = require("../../images/spruce.jpg");
-const tree = {
-  age: "13",
-  condition: "good",
-  image: "tree123.jpg",
-  kindOfTree: "spruce",
-  location: { lat: 20.99835602, lng: 27.01502627 },
-  necessaryWorks: "[{cut:false}]",
-  radius: "11",
-  registrationNumber: "566121218754",
-  __v: 0,
-  _id: "6",
-};
 
 const CardTree = ({ contact, closeModal }) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
@@ -27,19 +15,32 @@ const CardTree = ({ contact, closeModal }) => {
     closeModal(true);
     dispatch(getAllUsers());
   };
+
+  const {
+    age,
+    condition,
+    image,
+    kindOfTree,
+    location,
+    necessaryWorks,
+    radius,
+    registrationNumber,
+  } = contact;
   return (
     <div className={s.card}>
       <img className={s.img} src={TreeImg} alt="" />
       <div>
-        <h2 className={s.title}>Kind of tree: "spruce"</h2>
-        <p className={s.text}>Radius: '11' m</p>
-        <p className={s.text}>Age: '40'</p>
-        <p className={s.text}>Condition: "good"</p>
-        <p className={s.text}>
+        <h2 className={s.title}>{kindOfTree}</h2>
+        <p className={s.text}>Radius: {radius} m</p>
+        <p className={s.text}>Age: {age}</p>
+        <p className={s.text}>Condition: {condition}</p>
+        {/* <p className={s.text}>
           Registration Number: "9ca7fa2c-627f-4d4c-9f06-5081d96d58bc"
+        </p> */}
+        <p className={s.text}>
+          Location: {location.lat}, {location.lng}
         </p>
-        <p className={s.text}>Location:</p>
-        <p className={s.text}> necessaryWorks</p>
+        <p className={s.text}> {necessaryWorks} </p>
       </div>
     </div>
   );
